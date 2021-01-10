9d4c37edafe358c23583c951def1b2388e9fdafd
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-01-10 22:35:10 +0000

    Added only prod dependencies

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,38 +1,5 @@
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middleware/error-handler";
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
 import mongoose from "mongoose";
-
-const app = express();
-// Telling the app that even though traffic is coming from the nginx proxy, to trust it as secure
-app.set("trust proxy", true);
-
-// Create the session
-app.use(cookieSession({ signed: false, secure: true }));
-
-// Parse the body
-app.use(json());
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// Only works because we've got express-async-errors imported, otherwise would need to pass
-// NotFoundError() to next()
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
