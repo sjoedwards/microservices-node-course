d8c652e24b21e9e22f37a1dba35a992f6df99922
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-01-18 17:49:46 +0000

    boilerplate route

--- ticketing/orders/src/app.ts
+++ ticketing/orders/src/app.ts
@@ -4,10 +4,10 @@ import express, { Response, Request } from "express";
 // Means we can throw errors inside of async functions
 import "express-async-errors";
 import { json } from "body-parser";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,10 +17,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 // Because we've imported express-async-errors, that means we can throw errors async
 app.all("*", async (req: Request, res: Response) => {
