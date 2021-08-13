44e0af4469fe6ca0f3137e205f087a60264955db
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-08-13 09:14:11 +0100

    459

--- ticketing/payments/src/routes/__test__/new.test.ts
+++ ticketing/payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 
+jest.mock("../../stripe");
+
 it("returns a 404 when purchasing an order that doesn`t exist", async () => {
   await request(app)
     .post("/api/payments")
@@ -42,5 +44,21 @@ it("returns a 400 when purchasing a cancelled order", async () => {
     .post("/api/payments")
     .set("Cookie", global.signin(order.userId))
     .send({ token: "12334", orderId: order.id })
+    .expect(400);
+});
+
+it("returns a 204 with valid inputs", async () => {
+  const order = Order.build({
+    id: Types.ObjectId().toHexString(),
+    userId: Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    version: 0,
+    price: 20,
+  });
+  await order.save();
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(order.userId))
+    .send({ token: "tok_visa", orderId: order.id })
     .expect(400);
 });
