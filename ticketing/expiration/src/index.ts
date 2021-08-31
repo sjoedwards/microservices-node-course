bdba69875432c037eb1007f3034f9051a2eb3822
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-08-31 22:45:28 +0100

    added new deployment pipelines

--- ticketing/expiration/src/index.ts
+++ ticketing/expiration/src/index.ts
@@ -2,6 +2,8 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("Starting up...");
+
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
